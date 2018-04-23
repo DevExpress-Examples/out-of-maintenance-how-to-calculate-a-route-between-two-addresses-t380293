384a22f91053ed04f572d235378724cd517b5e3c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CalculateRoutesByAddresses/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.Xpf.Map;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace CalculateRoutesByAddresses {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e) {
+            routeProvider.CalculateRoute(new List<RouteWaypoint> {
+                new RouteWaypoint("New York", "Belmont Park, New York, USA"),
+                new RouteWaypoint("Las Vegas", "Lorenzi Park, Las Vegas, USA")
+            });
+        }
+
+        private void OnRouteCalculated(object sender, BingRouteCalculatedEventArgs e) {
+            SearchBoundingBox box = e.CalculationResult.RouteResults[0].BoundingBox;
+            GeoPoint topLeft = new GeoPoint {
+                Latitude = box.NorthLatitude,
+                Longitude = box.WestLongitude
+            };
+            GeoPoint bottomRight = new GeoPoint {
+                Latitude = box.SouthLatitude,
+                Longitude = box.EastLongitude
+            };
+            mapControl.ZoomToRegion(topLeft, bottomRight, 0.4);
+            waitIndicator.DeferedVisibility = false;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to: Calculate a Route Between Two Addresses
+
+
+This example demonstrates how to calculate a route between two addresses.
+
+
+<h3>Description</h3>
+
+To send a route request, use the <a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteDataProvider_CalculateRoutetopic">BingRouteDataProvider.CalculateRoute</a> method. This method receives a list of <a href="https://documentation.devexpress.com/#WindowsForms/clsDevExpressXtraMapRouteWaypointtopic">RouteWaypoint</a> objects as a parameter. Note that <a href="https://documentation.devexpress.com/#WindowsForms/clsDevExpressXtraMapRouteWaypointtopic">RouteWaypoint</a> objects are created using the constructor that receives the waypoint description and a keyword that is used to search a location on a map.
+
+<br/>
+
+
